df0009ac6b67235a7469696f5157da7583f7f5ad
Matthew Lawson <mlaws117@gmail.com>
2019-09-15 00:22:23 +0100

    connect node to react

--- server/src/index.js
+++ server/src/index.js
@@ -8,21 +8,23 @@ var wol = require('node-wol');
 
 // define the Express app
 const app = express();
+app.use(express.static(`${__dirname}/../../build`));
+const path = require('path')
 
 // the database
-const questions = [];
+//const questions = [];
 
 // enhance your app security with Helmet
-app.use(helmet());
+//app.use(helmet());
 
 // use bodyParser to parse application/json content-type
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 // enable all CORS requests
-app.use(cors());
+//app.use(cors());
 
 // log HTTP requests
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 //app.use(wol);
 
 // retrieve all questions
@@ -36,7 +38,7 @@ app.use(morgan('combined'));
 //   res.send(qs);
 // });
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '../../public/index.html');
+  res.sendFile(__dirname + '../../build/index.html');
 });
 app.get('/wake', (req, res) => {
     console.log("ping")
@@ -50,12 +52,16 @@ app.get('/wake', (req, res) => {
   });
 
 // get a specific question
-app.get('/:id', (req, res) => {
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
-  res.send(question[0]);
-});
+// app.get('/:id', (req, res) => {
+//   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+//   if (question.length > 1) return res.status(500).send();
+//   if (question.length === 0) return res.status(404).send();
+//   res.send(question[0]);
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
+})
 
 // start the server
 app.listen(8081, () => {
